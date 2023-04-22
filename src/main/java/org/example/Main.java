b7d18bdec3f118f373ecc8f025de4d692b22481a
Kajemix <adriancierocki97@gmail.com>
2023-04-22 12:35:06 +0200

    zmiany kurwa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Osoba osoba = new Osoba("Adam", "Kowalski");
         System.out.println(osoba.getInfo());
-<<<<<<< HEAD
         System.out.println(osoba.getInfo());
         Osoba osoba2 = new Osoba("Amelia", "Nowak");
         System.out.println(osoba2.getInfo());
         System.out.println("Hello");
-=======
-        Osoba osoba2 = new Osoba("Amelia", "Nowak");
-        System.out.println(osoba2.getInfo());
->>>>>>> da202aae45fedd78eea665e54d0dd65714eeb66d
+        System.out.println("Co jest 5?");
     }
 }
